88de2009350824fe1b134365393f801e41bfa492
Nik <niklas_fernandez@comcast.net>
2021-12-17 03:54:31 -0600

    Modifies routes and adds urls.py to tweets app.

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from tweets.views import home_view
+from tweets import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view),
+    path('', include(urls)),
 ]

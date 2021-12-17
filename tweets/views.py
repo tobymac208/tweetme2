ccbe0ea7d5a6bea5b99b7bf3dce953666c88136a
Nik <niklas_fernandez@comcast.net>
2021-12-17 04:11:28 -0600

    Update detail page with actual content.

--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Tweet
+
 
 def home_view(request, *args, **kwargs):
     return HttpResponse("<h1>Hello, world!</h1>")
+
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    reponse = ''
+
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        response = f'Details for {tweet_id} are {obj.content}'
+    except:
+        response = 'not found'
+    return HttpResponse(f'<h1>{response}</h1>')

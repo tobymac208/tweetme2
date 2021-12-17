069549e5797b205414fbfac7ace98bbdbfc5696f
Nik <niklas_fernandez@comcast.net>
2021-12-17 04:16:57 -0600

    Gives user standard 404 page.

--- tweets/views.py
+++ tweets/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
 from .models import Tweet
@@ -15,5 +15,5 @@ def tweet_detail_view(request, tweet_id, *args, **kwargs):
         obj = Tweet.objects.get(id=tweet_id)
         response = f'Details for {tweet_id} are {obj.content}'
     except:
-        response = 'not found'
+        raise Http404
     return HttpResponse(f'<h1>{response}</h1>')
